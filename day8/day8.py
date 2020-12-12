0be65640a3c00109682872245ab921c58d0390bc
kelectro <kiagkons@gmail.com>
2020-12-12 08:23:08 +0100

    Clean up

--- day8/day8.py
+++ day8/day8.py
@@ -1,14 +1,14 @@
 import copy
 filepath = '/home/kiagkons/Desktop/advent of code/day8/day8.in'
 
+
+#load the data in lists. every list has ['command', 'quantity']
 def load_data(path):
     file = open(filepath,'r')
     data = file.readlines()
     file.close()
     data = [data.strip() for data in data ]
     data = [data.split() for data in data]
-    # print(data[1][0])
-    # print(data)
     return(data)
 
 
@@ -20,21 +20,16 @@ def compiler(data):
     visited = []
     accumulator = 0
     i=0
-    
+
+    #assign command and quantity to different values to make it easir to work with
     for cmds in data:
-        # print(cmds[0])
         command.append(cmds[0])
         qunatity.append(cmds[1:])
-    # print(len(command))
-    
-
-    # for i in range(len(command)):
-        # print(i)
 
     while i not in visited and terminated == False  :    
-        print('i =',i)
-        print('command',command[i])
+        #keep track of visited commands
         visited.append(i)
+
         if command[i] == 'nop':
             i +=1
             pass
@@ -43,45 +38,38 @@ def compiler(data):
             i +=1
         elif command[i] == 'jmp':
             i += int(qunatity[i][0])
-            # print(i)
+
         if i == len(data):
-            print('Terminated')
+            # we reached the end of file
             terminated = True
+        
+        if i > len(data):
+            raise  ValueError("List index exhaustion, no possible solution")
+
     return(terminated,accumulator)
 
-    # print(accumulator)
 
 
-def part2(instructions):
-    # print(data)
-    new_data =[]
-
-    for i in range(len(instructions)):
-        if instructions[i][0] == 'nop':
-            instructions[i][0] = 'jmp'
-        # compiler(instructions)
-        elif instructions[i][0] == 'jmp':
-            instructions[i][0] = 'nop'
-        compiler(instructions)
         
-def part2b(instructions):
+def part2(instructions):
+    #save original commands
     new_instructions = copy.deepcopy(instructions)
     terminated = False
     index = 0
-    # print(new_instructions[10][0])
     
     while terminated == False : 
-        print('2b index',index)       
         if new_instructions[index][0] == 'nop':
             new_instructions[index][0] = 'jmp'
         elif new_instructions[index][0] == 'jmp':
             new_instructions[index][0] = 'nop'
         terminated,result = compiler(new_instructions)
-        # print('\n',new_instructions)
+        #replace modded list with original in order to try next possible jmp<-->nop switch
         new_instructions = copy.deepcopy(instructions)
         index +=1
-    print('Terminated result',result)
+    return(result)
 
         
-# compiler(load_data(filepath))
-part2b(load_data(filepath))
+acc1 = compiler(load_data(filepath))
+print('Last known value for accumulator before the infinite loop was : ',acc1[1])
+result = part2(load_data(filepath))
+print('The program has safely terminated. Last accumulator value is: ', result)
